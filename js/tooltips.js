1d88ecc450fef75303aaf4dcc0761940f0a45683
Edmond Leung <leung.edmond@gmail.com>
2009-10-21 11:18:46 +0000

    Improved hooks. Added mirroring. Fixed overflow bug. Added ability to update tooltips. Added ability to do shadowme. Fixed mouse relative positioning bug.

--- js/tooltips.js
+++ js/tooltips.js
@@ -1,37 +1,63 @@
+// Public methods:
+// ------------------------------
+// tooltip.reload();                // Updates the tooltips
+// tooltip.getToolTipContainer();   // Returns the dom element you can apply ShadowMe to
+
 var ToolTips = Class.create({
     initialize: function(selectors, options) {
         this.options = Object.extend(Object.extend({ }, ToolTips.DefaultOptions), options || { });
         
-        this.elements = $$(selectors);
+        this.targets = $$(selectors);
         
         // Attach mouse over and mouse out events
-        this.elements.each(function(element) {
-            // Store title inside tooltip variable inside of the element and remove default tooltip
-            element.tooltip = element.title;
-            element.title = "";
+        this.targets.each(function(target) {
+            // Store title inside tooltip variable inside of the target and remove default tooltip
+            target.tooltip = target.title;
+            target.title = "";
             
-            element.observe('mousemove', this.updatePosition.bindAsEventListener(this));
-            
-            element.observe('mouseover', function(event) {
+            target.observe('mouseover', function(event, target) {
                 clearTimeout(this.delayTimer);
-                this.delayTimer = setTimeout(this.showToolTip.bind(this, event, element), this.options.showDelay)
-            }.bind(this));
+                this.delayTimer = setTimeout(this.showToolTip.bind(this, event, target), this.options.showDelay)
+            }.bindAsEventListener(this, target));
             
-            element.observe('mouseout', function() {
+            target.observe('mouseout', function() {
                 clearTimeout(this.delayTimer);
-                this.hideToolTip(element);
+                this.hideToolTip(target);
             }.bind(this));
         }.bind(this));
         
         this.generateToolTip();
+        
+        // Apply mouse move to whole document   
+        if (!this.options.fixed) {
+            $(document.body).observe('mousemove', this.storeMousePosition.bindAsEventListener(this));
+            $(document.body).observe('mousemove', this.updatePosition.bind(this));
+        }
+    },
+    
+    // Call this externally to reload the tooltips
+    reload: function() {
+        // Reload the tooltips
+        this.targets.each(function(target) {
+            // Only update if changed
+            if (target.title != "") {
+                target.tooltip = target.title;
+                target.title = "";
+            }
+        });
+    },
+    
+    // Returns the tooltip container that you can apply ShadowMe to
+    getToolTipContainer: function() {
+        return this.toolTipContainer;
     },
     
     generateToolTip: function() {
         // Create dom elements
-        this.toolTipContainer = new Element("div", { "class": this.options.containerClass });
-        this.toolTipTitle = new Element(this.options.titleTagName, { "class": this.options.titleClass });
-        this.toolTipContent = new Element("div", { "class": this.options.contentClass });
-        this.toolTipStem = new Element("div", { "class": this.options.stemClass });
+        this.toolTipContainer   = new Element("div", { "class": this.options.containerClass });
+        this.toolTipTitle       = new Element(this.options.titleTagName, { "class": this.options.titleClass });
+        this.toolTipContent     = new Element("div", { "class": this.options.contentClass });
+        this.toolTipStem        = new Element("div", { "class": this.options.stemClass });
         
         // Attach to each other
         this.toolTipContainer.insert(this.toolTipTitle);
@@ -39,9 +65,7 @@ var ToolTips = Class.create({
         this.toolTipContainer.insert(this.toolTipStem);
         
         // Position absolutely
-        this.toolTipContainer.setStyle({
-            position: "absolute"
-        });
+        this.toolTipContainer.setStyle({ position: "absolute" });
         
         // Hide by default
         this.toolTipContainer.hide();
@@ -50,55 +74,165 @@ var ToolTips = Class.create({
         $(document.body).insert(this.toolTipContainer);
     },
     
-    updatePosition: function(event, element) {
-        var toolTipSize = [ this.toolTipContainer.offsetWidth, this.toolTipContainer.offsetHeight ];
-        var offset, position;
-        
-        if (this.options.fixed) {
-            if (element == null) return;
-            
-            var elementSize = [ element.offsetWidth, element.offsetHeight ];
-            
-            offset = element.cumulativeOffset();
-            position = [ offset[0], offset[1] ];
-            
-            // Top/bottom hooks
-            if (this.options.hooks.include('bottom')) {
-                position[1] = offset[1] + elementSize[1];   
-            }
-
-            // Left/right hooks
-            if (this.options.hooks.include('right')) {
-                position[0] = offset[0] + elementSize[0];   
-            } else if (this.options.hooks.include('left')) {
-                position[0] = offset[0] - toolTipSize[0];
-            }
-        } else {
-            if (event == null) return;
-            
-            offset = [ Event.pointerX(event), Event.pointerY(event) ];
-            position = [ offset[0], offset[1] ];
-        }
-        
-        if (this.options.hooks.include('top')) {
-            position[1] = offset[1] - toolTipSize[1];
-        }
-        
-        if (this.options.hooks.include('left')) {
-            position[0] = offset[0] - toolTipSize[0];
-        }
-        
-        // Offsets from options
-        position[0] += this.options.offset.x;
-        position[1] += this.options.offset.y;
-        
-        this.toolTipContainer.setStyle({
-            left: position[0] + "px", top: position[1] + "px"
-        });
+    storeMousePosition: function(event) {
+        this.mouse = { x: Event.pointerX(event), y: Event.pointerY(event) };
     },
     
-    showToolTip: function(event, element) {        
-        var data = element.tooltip.split(this.options.delimiter);
+    updatePosition: function(target) {
+        // Store old display and opacity so we can revert back to them once position is done
+        oldDisplay = this.toolTipContainer.getStyle('display');
+        oldOpacity = this.toolTipContainer.getOpacity();
+        
+        // Show, but make transparent so that we can grab the size
+        this.toolTipContainer.setOpacity(0);
+        this.toolTipContainer.show();
+        
+        // Position to start hooks from
+        var startingPosition;
+        
+        // Copy so we can manipulate
+        var offset = Object.extend({}, this.options.offset);
+        var hook = Object.extend({}, this.options.hook);
+        
+        // Make lower case for easier regex
+        hook.target = hook.target.toLowerCase();
+        hook.tip = hook.tip.toLowerCase();
+        
+        var update = function() {};
+        var update = function(level) {
+            // Starting level is 0 (this is so we don't run into an infinite loop)
+            if (level == undefined) level = 0;
+            if (level > 1) return;
+            
+            if (this.options.fixed) {
+                // Grab size of target element
+                var targetSize = { width: target.offsetWidth, height: target.offsetHeight };
+
+                // Begin with the position of the target
+                var tempOffset = target.cumulativeOffset();
+                startingPosition = { x: tempOffset[0], y: tempOffset[1] };
+                
+                // Adjust positioing for scrolled parents 
+                var t = $(target.parentNode);
+                while (t != null && t.tagName != 'body') {
+                    if (t.scrollLeft != undefined) startingPosition.x -= t.scrollLeft;
+                    if (t.scrollTop != undefined) startingPosition.y -= t.scrollTop;
+                    t = $(t.parentNode);
+                }
+
+                // Lowercase target hooking
+                var targetHook = hook.target.toLowerCase();
+
+                // Perform target hooking
+                if (targetHook.match('bottom')) startingPosition.y += targetSize.height;
+                if (targetHook.match('right')) startingPosition.x += targetSize.width;
+
+                // Middle target hooking
+                if (targetHook == "bottom" || targetHook == "top") startingPosition.x += parseInt(targetSize.width / 2.0);
+                if (targetHook == "right" || targetHook == "left") startingPosition.y += parseInt(targetSize.height / 2.0);
+            } else {
+                // Begin from where the mouse is
+                startingPosition = { x: this.mouse.x, y: this.mouse.y };
+            }
+
+            // Grab size of tooltip
+            var toolTipSize = { width: this.toolTipContainer.offsetWidth, height: this.toolTipContainer.offsetHeight };
+
+            // Lowercase tip hooking
+            var tipHook = hook.tip.toLowerCase();
+            
+            // Perform tips hooking
+            if (tipHook.match('bottom')) startingPosition.y -= toolTipSize.height;
+            if (tipHook.match('right')) startingPosition.x -= toolTipSize.width;
+
+            // Middle target hooking
+            if (tipHook == "bottom" || tipHook == "top") startingPosition.x -= parseInt(toolTipSize.width / 2.0);
+            if (tipHook == "right" || tipHook == "left") startingPosition.y -= parseInt(toolTipSize.height / 2.0);
+
+            // Do offset
+            startingPosition.x += offset.x;
+            startingPosition.y += offset.y;  
+            
+            // Mirror if required
+            var dimensions = document.viewport.getDimensions();
+            var scrollOffset = document.viewport.getScrollOffsets();
+            
+            var viewPortBounds = {
+                top:    scrollOffset.top,
+                right:  scrollOffset.left + dimensions.width,
+                bottom: scrollOffset.top + dimensions.height,
+                left:   scrollOffset.left
+            };
+            
+            var toolTipBounds = {
+                top:    startingPosition.y,
+                right:  startingPosition.x + toolTipSize.width,
+                bottom: startingPosition.y + toolTipSize.height,
+                left:   startingPosition.x
+            };
+            
+            // Remove mirror class first (reset mirror state) - only remove if first check
+            if (level == 0) {
+                this.toolTipContainer.removeClassName(this.options.mirrorClass);
+                this.toolTipContainer.removeClassName(this.options.verticalMirrorClass);
+                this.toolTipContainer.removeClassName(this.options.horizontalMirrorClass);
+            }
+            
+            // Check to left/right mirroring
+            if (toolTipBounds.right > viewPortBounds.right || toolTipBounds.left < viewPortBounds.left) {
+                if (hook.tip.match('right')) {
+                    hook.tip = hook.tip.replace(/right/, 'left');
+                } else if (hook.tip.match('left')) {
+                    hook.tip = hook.tip.replace(/left/, 'right'); 
+                }
+                
+                if (hook.target.match('right')) {
+                    hook.target = hook.target.replace(/right/, 'left');  
+                }  else if (hook.target.match('left')) {
+                    hook.target = hook.target.replace(/left/, 'right');
+                }
+                
+                offset.x = -offset.x;
+               
+                this.toolTipContainer.addClassName(this.options.mirrorClass);
+                this.toolTipContainer.addClassName(this.options.horizontalMirrorClass);
+            }
+            
+            // Check to top/bottom mirroring
+            if (toolTipBounds.bottom > viewPortBounds.bottom || toolTipBounds.top < viewPortBounds.top) {
+                if (hook.tip.match('bottom')) {
+                    hook.tip = hook.tip.replace(/bottom/, 'top');
+                } else if (hook.tip.match('top')) {
+                    hook.tip = hook.tip.replace(/top/, 'bottom'); 
+                }
+                
+                if (hook.target.match('bottom')) {
+                    hook.target = hook.target.replace(/bottom/, 'top');  
+                }  else if (hook.target.match('top')) {
+                    hook.target = hook.target.replace(/top/, 'bottom');
+                }
+                
+                offset.y = -offset.y;
+                
+                this.toolTipContainer.addClassName(this.options.mirrorClass);
+                this.toolTipContainer.addClassName(this.options.verticalMirrorClass);
+            }
+
+            update(++level);
+        }.bind(this);
+        
+        update();
+        
+        this.toolTipContainer.setStyle({
+            left: startingPosition.x + "px", top: startingPosition.y + "px"
+        });
+        
+        this.toolTipContainer.setStyle({ display: oldDisplay })
+        this.toolTipContainer.setOpacity(oldOpacity);
+    },
+    
+    showToolTip: function(event, target) {        
+        var data = target.tooltip.split(this.options.delimiter);
         var title = data[0];
         var content = data[1];
         
@@ -106,10 +240,10 @@ var ToolTips = Class.create({
         this.toolTipContent.innerHTML = content;
         
         // Show, but make transparent so that we can grab the size
-        this.toolTipContainer.show();
         this.toolTipContainer.setOpacity(0);
+        this.toolTipContainer.show();
         
-        this.updatePosition(event, element);
+        this.updatePosition(target);
         
         // Fade or not
         if (this.options.fade) {
@@ -132,6 +266,11 @@ var ToolTips = Class.create({
 });
 
 ToolTips.DefaultOptions = {
+    // Mirror classes
+    mirrorClass:            "mirrored",
+    verticalMirrorClass:    "vertical",
+    horizontalMirrorClass:  "horizontal",
+    
     // Fixed does not follow the mouse
     fixed:              true, 
     
@@ -144,11 +283,9 @@ ToolTips.DefaultOptions = {
     // Fade config
     fade:               true,
     fadeSpeed:          300,
-    
-    // Hooks, to hook to the top, left, bottom or right (can only use bottom or top at the same time, and left or right at the same time)
-    // Add each hook into the array
-    hooks:              [ "bottom" ],
-    
+
+    // Works like prototip
+    hook:               { target: "bottom", tip: "top" },
     
     // Delimiter used to grab title and content
     delimiter:          ":",
@@ -160,5 +297,5 @@ ToolTips.DefaultOptions = {
     containerClass:     "tooltip-container",
     titleClass:         "tooltip-title",
     contentClass:       "tooltip-content",
-    stemClass:       "tooltip-stem"
+    stemClass:          "tooltip-stem"
 };
